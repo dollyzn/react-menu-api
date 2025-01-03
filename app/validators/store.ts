1638ee6774e2e86665091c7d8ff12a9c05df4b78
Natã <natasjesus1@gmail.com>
2025-01-03 02:07:26 -0300

    feat: enhance store validator by updating address, Instagram, and iFood URL validations; refine slug uniqueness check for updates

--- app/validators/store.ts
+++ app/validators/store.ts
@@ -2,32 +2,25 @@ import vine from '@vinejs/vine'
 import StoreStatus from '../enums/store-status.js'
 
 const name = vine.string().trim()
-const status = vine.enum(Object.values(StoreStatus)).optional()
-const address = vine.string().optional()
-const instagramUrl = vine.string().optional()
-const ifoodUrl = vine.string().optional()
-const bannerUrl = vine.string().optional()
-const photoUrl = vine.string().optional()
-const slug = vine
-  .string()
-  .regex(/^[a-z0-9-]+$/)
-  .unique(async (db, value) => {
-    const match = await db.from('stores').select('id').where('slug', value).first()
-
-    return !match
-  })
+const address = vine.string().trim().optional()
+const instagramUrl = vine.string().url().startsWith('https://www.instagram.com').optional()
+const ifoodUrl = vine.string().url().startsWith('https://www.ifood.com.br').optional()
 const isDefault = vine.boolean().optional()
 
 const storeValidator = vine.compile(
   vine.object({
     name,
-    status,
     address,
     instagramUrl,
     ifoodUrl,
-    bannerUrl,
-    photoUrl,
-    slug,
+    slug: vine
+      .string()
+      .regex(/^[a-z0-9-]+$/)
+      .unique(async (db, value) => {
+        const match = await db.from('stores').select('id').where('slug', value).first()
+
+        return !match
+      }),
     isDefault,
   })
 )
@@ -35,13 +28,23 @@ const storeValidator = vine.compile(
 const updateValidator = vine.compile(
   vine.object({
     name: name.optional(),
-    status,
     address,
     instagramUrl,
     ifoodUrl,
-    bannerUrl,
-    photoUrl,
-    slug: slug.optional(),
+    slug: vine
+      .string()
+      .regex(/^[a-z0-9-]+$/)
+      .unique(async (db, value, field) => {
+        const storeId = field.data.params.id
+        const match = await db
+          .from('stores')
+          .select('id')
+          .where('slug', value)
+          .andWhereNot('id', storeId)
+          .first()
+
+        return !match
+      }),
     isDefault,
   })
 )
